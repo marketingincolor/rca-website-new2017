4caefcd4398cdac89efc9d9be1d148fad465dc42
nbeers22 <nbeers22@gmail.com>
2017-10-03 11:46:40 -0400

    Removed single-view-all template file. Copied code to page-view-all. Fixed case_studies

--- themes/rca-inc/archive-case_studies.php
+++ themes/rca-inc/archive-case_studies.php
@@ -85,7 +85,7 @@ get_header(); ?>
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
 
-					echo '<div class="small-10 small-offset-1 medium-offset-0 medium-3 columns archive-row end" data-equalizer-watch>';
+					echo '<div class="small-10 small-offset-1 medium-offset-0 medium-3 columns archive-row end">';
 					get_template_part( 'template-parts/content', 'archive-blocks' );
 					echo '</div>';
 
