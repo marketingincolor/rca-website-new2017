0d13a2ae922a573993de94e4ac045a367e7a9403
nbeers22 <nbeers22@gmail.com>
2017-10-09 11:25:32 -0400

    Updated copy and styling of 404 page

--- themes/rca-inc/404.php
+++ themes/rca-inc/404.php
@@ -14,13 +14,13 @@ get_header(); ?>
 			<div class="row">
 				<div class="small-10 small-offset-1 columns">
 					<div class="row">
-						<div class="small-12 medium-3 medium-offset-3 columns text-center">
+						<div class="small-12 medium-3 medium-offset-2 columns text-center">
 							<img src="<?php echo get_stylesheet_directory_uri() . '/images/404.jpg';?>" alt="RCA - 404 Page">
 						</div>
-						<div class="small-12 medium-6 columns text-center medium-text-left">
+						<div class="small-12 medium-6 columns text-center medium-text-left end">
 							<section class="error-404 not-found">
 								<header class="page-header">
-									<h1 class="page-title"><?php esc_html_e( 'Oops! This page does not exist. Please check the URL or use our search bar to start over. 
+									<h1 class="page-title"><?php esc_html_e( 'Oops! This page does not exist. Please check the URL, use our search bar to start over, or click below to go back to our home page. 
 				', 'rca-inc' ); ?></h1>
 									<a href="<?php echo get_site_url(); ?>"><?php get_template_part('template-parts/button', 'back-to-home' ); ?></a>
 								</header><!-- .page-header -->
