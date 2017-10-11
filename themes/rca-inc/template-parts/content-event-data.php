6853fff6b8eb406c7181f484624cfbf3c6bdfd79
Adam Doe <adoe@marketingincolor.com>
2017-10-11 15:00:02 -0400

    Updated events pages for desktop to show correct icons and formatting

--- themes/rca-inc/template-parts/content-event-data.php
+++ themes/rca-inc/template-parts/content-event-data.php
@@ -7,13 +7,13 @@ $event_time     = get_field('event_time');
 	<div class="row" data-equalizer>
 		<div id="related-content-block" class="small-10 small-offset-1 columns">
 			<div class="small-12 medium-4 columns random-item" data-equalizer-watch>
-				<i class="fa fa-calendar" aria-hidden="true"></i><?php the_field('event_date'); ?>
+				<i class="fa fa-calendar" aria-hidden="true"></i><p><?php the_field('event_date'); ?></p>
 			</div>
 			<div class="small-12 medium-4 columns random-item" data-equalizer-watch>
-				<i class="fa fa-calendar" aria-hidden="true"></i><?php the_field('event_location'); ?>
+				<i class="fa fa-globe" aria-hidden="true"></i><p><?php the_field('event_location'); ?></p>
 			</div>
 			<div class="small-12 medium-4 columns random-item" data-equalizer-watch>
-				<i class="fa fa-calendar" aria-hidden="true"></i><?php the_field('event_time'); ?>
+				<i class="fa fa-clock-o" aria-hidden="true"></i><p><?php the_field('event_time'); ?></p>
 			</div>
 		</div>
 	</div>
