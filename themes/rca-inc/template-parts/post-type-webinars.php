d252e1b3abbce453137cdfff9efcd64add94f2af
Adam Doe <adoe@marketingincolor.com>
2017-10-12 14:41:00 -0400

    Oops, put that back

--- themes/rca-inc/template-parts/post-type-webinars.php
+++ themes/rca-inc/template-parts/post-type-webinars.php
@@ -14,10 +14,7 @@ $people = get_field('who_will_benefit');
 $webinar_form_title = get_field('webinar_form_title');
 $webinar_form_copy = get_field('webinar_form_copy');
 $webinar_title = get_field('webinar_title');
-$presenters = get_field('your_presenters');
-echo '<pre>';
-var_dump($presenters);
-echo '</pre>';
+$presenters = get_field('presenters');
 //$presenters = explode(',', $presenters);
 // Header BG
 $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); 
