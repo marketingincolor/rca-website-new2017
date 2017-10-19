7a38eb5761791e7fe23c41e65fc0bc4bf693e8e6
nbeers22 <nbeers22@gmail.com>
2017-10-19 16:32:57 -0400

    Dresing up agent services form

--- themes/rca-inc/page-pharmaceutical.php
+++ themes/rca-inc/page-pharmaceutical.php
@@ -37,10 +37,10 @@ get_header(); ?>
 			'menu_name'   => 'Pharmaceutical Regulatory Affairs',
 			'slug' => 'regulatory-affairs'
 		),
-		'quality_assurance' => array(
-			'title' => 'Quality Assurance',
-			'menu_name'   => 'Quality Assurance',
-			'slug' => 'quality-assurance'
+		'strategic_consulting' => array(
+			'title' => 'Strategic Consulting',
+			'menu_name'   => 'Pharmaceutical Strategic Consulting',
+			'slug' => 'strategic-consulting'
 		),
 		'compliance_assurance' => array(
 			'title' => 'Compliance Assurance',
@@ -52,10 +52,10 @@ get_header(); ?>
 			'menu_name'   => 'Pharmaceutical Remediation Strategy and Support',
 			'slug' => 'remediation-strategy-support'
 		),
-		// 'strategic_consulting' => array(
-		// 	'title' => 'Strategic Consulting',
-		// 	'menu_name'   => 'Pharmaceutical Strategic Consulting',
-		// 	'slug' => 'strategic-consulting'
+		// 'quality_assurance' => array(
+		// 	'title' => 'Quality Assurance',
+		// 	'menu_name'   => 'Quality Assurance',
+		// 	'slug' => 'quality-assurance'
 		// ),
 	);
 
@@ -91,7 +91,7 @@ get_header(); ?>
 							// 	echo '</div>';
 							// endif;
 
-							if($service['menu_name'] == 'Pharmaceutical Regulatory Affairs' || $service['menu_name'] == 'Pharmaceutical Compliance Assurance' || $service['menu_name'] == 'Pharmaceutical Remediation Strategy and Support' || $service['menu_name'] == 'Pharmaceutical Quality Assurance' ):
+							if($service['menu_name'] == 'Pharmaceutical Regulatory Affairs' || $service['menu_name'] == 'Pharmaceutical Compliance Assurance' || $service['menu_name'] == 'Pharmaceutical Remediation Strategy and Support' || $service['menu_name'] == 'Pharmaceutical Strategic Consulting' ):
 								echo '<div class="text-left service-menu-items">';
 								echo wp_nav_menu( array('menu' => $service['menu_name']));
 								echo '</div>';
@@ -114,25 +114,25 @@ get_header(); ?>
 				<!-- Hardcoded Strategic Consulting  -->
 				<div class="small-10 small-offset-1 medium-12 medium-offset-0 columns text-center">
 					<div class="service-block">
-						<div class="service-icon"><img src="<?php echo get_service_image('Strategic Consulting'); ?>" alt=""></div>
-						<div class="service-title"><a href="<?php echo home_url('pharmaceutical/strategic-consulting'); ?>">Strategic Consulting</a></div>
+						<div class="service-icon"><img src="<?php echo get_service_image('Quality Assurance'); ?>" alt=""></div>
+						<div class="service-title"><a href="<?php echo home_url('pharmaceutical/quality-assurance'); ?>">Quality Assurance</a></div>
 					</div>
 					<div class="text-left service-menu-items">
-						<?php echo wp_nav_menu(array('menu'=> 'Pharmaceutical Strategic Consulting')); ?>
+						<?php //echo wp_nav_menu(array('menu'=> 'Pharmaceutical Quality Assurance')); ?>
 					</div>
 				</div>
-				<!-- /Hardcoded Strategic Consulting -->
+				<!-- /Hardcoded Quality Assurance -->
 			</div>
  
 			<div class="show-for-medium medium-4 columns">
-				<!-- Hardcoded Strategic Consulting  -->
+				<!-- Hardcoded Quality Assurance  -->
 				<div class="small-10 small-offset-1 medium-12 medium-offset-0 columns text-center">
 					<div class="service-block">
-						<div class="service-icon"><img src="<?php echo get_service_image('Strategic Consulting'); ?>" alt=""></div>
-						<div class="service-title"><a href="<?php echo home_url('pharmaceutical/strategic-consulting'); ?>">Strategic Consulting</a></div>
+						<div class="service-icon"><img src="<?php echo get_service_image('Quality Assurance'); ?>" alt=""></div>
+						<div class="service-title"><a href="<?php echo home_url('pharmaceutical/quality-assurance'); ?>">Quality Assurance</a></div>
 					</div>
 					<div class="text-left service-menu-items">
-						<?php echo wp_nav_menu(array('menu'=> 'Pharmaceutical Strategic Consulting')); ?>
+						<?php //echo wp_nav_menu(array('menu'=> 'Pharmaceutical Quality Assurance')); ?>
 					</div>
 				</div>
 				<!-- /Hardcoded Strategic Consulting -->
