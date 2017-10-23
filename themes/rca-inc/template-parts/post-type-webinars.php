7103730610e7eb2af3e608b61dfca756b26f0dd7
Adam Doe <adoe@marketingincolor.com>
2017-10-23 15:19:11 -0400

    Updated pre webinar page form format

--- themes/rca-inc/template-parts/post-type-webinars.php
+++ themes/rca-inc/template-parts/post-type-webinars.php
@@ -98,19 +98,13 @@ $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 
 			</div>
 		</div>
 	</div>
-	<style>
-		#webinar-form-block h2{
-			font-size: 2rem;
-			color: white;
-			font-style: italic;
-			font-family: 'lora';
-		}
-	</style>
+
+
 	<!-- FORM FOR MEDIUM UP -->
 	<?php if(get_field('pre_webinar_ss_form')): ?>
-	<div id="webinar-form-block" class="show-for-medium">
+	<div id="webinar-form-block" class="">
 		<div class="row">
-			<div id="form" class="small-10 small-offset-1 columns text-center">
+			<div id="form" class="small-12 medium-10 medium-offset-1 columns text-center">
 
 
 				<!-- SHARPSPRING FORM GOES HERE -->
@@ -157,82 +151,9 @@ $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 
 	<!-- /Content -->
 </div>
 
-<!-- HIDDEN SHARE MENU -->
-<div id="share-menu" style="display:none;">
-	<div class="row close">
-		<div class="small-offset-10 small-2 columns">
-			<i class="fa fa-times fa-2x" aria-hidden="true"></i>
-		</div>
-	</div>
-	<div class="row title">
-		<div class="small-12 columns">
-			<h1>Share This Page</h1>
-		</div>
-	</div>
-	<div id="social-icons">
-		<div class="row a2a_kit a2a_kit_size_32 a2a_default_style text-center">
-			<div class="small-2 columns text-center">
-				<a class="a2a_button_facebook"><i class="fa fa-facebook-square fa-3x a2a_button_facebook" aria-hidden="true"></i></a>
-			</div>
-			<div class="small-2 columns text-center">
-				<a class="a2a_button_linkedin"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
-			</div>
-			<div class="small-2 columns text-center">
-				<a class="a2a_button_googleplusone"><i class="fa fa-google-plus-square fa-3x" aria-hidden="true"></i></a>
-			</div>
-			<div class="small-2 columns text-center">
-				<a class="a2a_button_twitter"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
-			</div>
-			<div class="small-2 columns text-center">
-				<a class="a2a_button_email"><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a>
-			</div>
-		</div>
-	</div>
-</div>
-
-<!-- /HIDDEN SHARE MENU -->
-	<!-- For Custom Share Buttons -->
-	<script async src="https://static.addtoany.com/menu/page.js"></script>
-	<!-- SHARE BUTTON CLICKS -->
-	<script>
-		$(document).ready(function() {
-			var logo = $('#masthead > section.hide-for-large > div:nth-child(1)').height();
-			var close = $('#share-menu .close');
-			logo = Math.abs(logo);
-			console.log( 'logoheight ' + logo);
-			var shareButton = $('#share-block');
-			var shareMenu = $('#share-menu');
-
-			shareButton.on('click', function() {
-
-				// Show the Menu
-				shareMenu.show();
-				shareMenu.toggleClass('share-menu-js');
-				shareMenu.css('top', logo); 
-
-				// Disable scrolling
-				$('html, body').css( { 
-					overflow:'hidden', 
-					height: '100%'
-				});
-			});
-
-			close.on('click', function() {
-
-				//Enable scrolling
-				$('html, body').css({
-				    overflow: 'auto',
-				    height: 'auto'
-				});
-
-				// Hide the Menu
-				shareMenu.toggleClass('share-menu-js');
-				shareMenu.hide();
-			});
-
-
-		})
-	</script>
+	<!-- SHARE MENU -->
+	<?php get_template_part('template-parts/hidden', 'share-menu'); ?>
+	<!-- /SHARE MENU -->
 
 	<!-- Form Animations -->
 	<script>
