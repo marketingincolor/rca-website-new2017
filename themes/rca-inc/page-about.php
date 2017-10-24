2163f63536babc7d5781b44253297080a9f960a7
Adam Doe <adoe@marketingincolor.com>
2017-10-24 16:15:59 -0400

    Added comments to about page

--- themes/rca-inc/page-about.php
+++ themes/rca-inc/page-about.php
@@ -1,29 +1,35 @@
 <?php
 
+/**
+ * Purpose: For displaying the about page
+ * Date: 10/24/2017
+ * Author: AD.,NB.,ET., MARKETING IN COLOR
+ */
+
 global $post;
 $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 $testimonial   = get_field('testimonial');
 
 get_header(); ?>
 	
-	<!-- Featured Image -->
+	<!-- FEATURED IMAGE -->
 	<div id="featured-img-wrapper" class="row expanded">
 		<div id="featured-img" style="background: linear-gradient(rgba(196,97,43, 0.7), rgba(196,97,43, 0.7)),
 	            rgba(196,97,43,0.7) url('<?php echo $backgroundImg[0]; ?>'); background-size: cover;">
 				<div class="featured-img-title"><h1><?php the_title(); ?></h1></div>
 		</div>
 	</div>
-	<!-- / Featured Image -->
+	<!-- / FEATURED IMAGE -->
 
 	<?php get_template_part( 'template-parts/section', 'breadcrumbs-social'); ?>
 
-	<!-- The Content -->
+	<!-- THE CONTENT -->
 	<div id="the-content" class="row">
 		<div class="small-10 small-offset-1 columns">
 			<?php echo the_content(); ?>
 		</div>
 	</div>
-	<!-- /The Content -->
+	<!-- /THE CONTENT -->
 
 	<!-- TESTIMONAIAL -->
 	<div class="row">
@@ -44,5 +50,4 @@ get_header(); ?>
 	<!-- /NEWS -->
 
 <?php
-//get_sidebar();
 get_footer();
