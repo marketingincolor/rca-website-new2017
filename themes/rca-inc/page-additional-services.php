2f1b2b60bddbf380565d1e74cb3baa8f83abeddd
Adam Doe <adoe@marketingincolor.com>
2017-10-24 16:14:38 -0400

    Cleaned up additional services page

--- themes/rca-inc/page-additional-services.php
+++ themes/rca-inc/page-additional-services.php
@@ -1,33 +1,39 @@
 <?php
 
+/**
+ * Purpose: For displaying the additional services page
+ * Date: 10/24/2017
+ * Author: AD.,NB.,ET., MARKETING IN COLOR
+ */
+
 global $post;
 $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 
 get_header(); ?>
 	
-	<!-- Featured Image -->
+	<!-- FEATURED IMAGE -->
 	<div id="featured-img-wrapper" class="row expanded">
 		<div id="featured-img" style="background: linear-gradient(rgba(196,97,43, 0.7), rgba(196,97,43, 0.7)),
 	            rgba(196,97,43,0.7) url('<?php echo $backgroundImg[0]; ?>'); background-size: cover;">
 				<div class="featured-img-title"><h1><?php the_title(); ?></h1></div>
 		</div>
 	</div>
-	<!-- / Featured Image -->
+	<!-- /FEATURED IMAGE -->
 
 	<div class="page-wrapper">
 		
 		<?php get_template_part( 'template-parts/section', 'breadcrumbs-social'); ?>
 
-		<!-- The Content -->
+		<!-- THE CONTENT -->
 		<div class="row text-center">
 			<div class="small-10 small-offset-1 medium-12 medium-offset-0 columns text-center">
 				<?php the_content(); ?>
 			</div>
 		</div>
-		<!-- /The Content -->
+		<!-- /THE CONTENT -->
 
 
-		<!-- List of Services -->
+		<!-- LIST OF SERVICES -->
 		<?php
 
 		$services_array = array(
@@ -49,45 +55,40 @@ get_header(); ?>
 		);
 
 		?>
-		<!-- /List of Services -->
+		<!-- /LIST OF SERVICES -->
 
 		<div id="additional-services" class="row">
 
-				<!-- Loop Through Services -->
-					<?php
-						$count = 1;
-						foreach($services_array as $service){
+			<!-- LOOP THROUGH THE SERVICES -->
+			<?php
+				$count = 1;
+				foreach($services_array as $service){
 
-								echo '<div class="small-10 small-offset-1 medium-4 medium-offset-0 columns text-center end" data-equalizer-watch>';
-								echo '<div class="service-block">';
-								echo '<a href="' . home_url( 'additional-services/' . $service['slug'] ) . '"><div class="service-icon"><img src="' . get_service_image($service['title']) . '"/></div>';
-								echo '<div class="service-title">' . $service['title'] . '</a></div>';
-								echo '</div>';
+						echo '<div class="small-10 small-offset-1 medium-4 medium-offset-0 columns text-center end" data-equalizer-watch>';
+						echo '<div class="service-block">';
+						echo '<a href="' . home_url( 'additional-services/' . $service['slug'] ) . '"><div class="service-icon"><img src="' . get_service_image($service['title']) . '"/></div>';
+						echo '<div class="service-title">' . $service['title'] . '</a></div>';
+						echo '</div>';
 
-							// If our menu array isn't empty display the menu.
-							// if($service['menu_name'] != '' || $service['menu_name'] != 'Strategic Consulting' ):
-							// 	echo '<div class="text-left service-menu-items">';
-							// 	echo wp_nav_menu( array('menu' => $service['menu_name']));
-							// 	echo '</div>';
-							// endif;
+					echo '</div>';
+				}
 
-							echo '</div>';
-						}
+			?>
+			<!-- /LOOP THROUGH THE SERVICES -->
 
-					?>
-
-				<!-- /Loop Through Services -->
 		</div>
+
 	</div> <!-- /.page-wrapper -->
 	
+	<!-- LEARN MORE FORM -->
 	<div id="contact-learn-more-wrapper">
 		<?php get_template_part('template-parts/section', 'learn-more-form-container-blue'); ?>
 	</div>
+	<!-- /LEARN MORE FORM -->
 
 	<!-- NEWS -->
 	<?php get_template_part('template-parts/section', 'news'); ?>
 	<!-- /NEWS -->
 
 <?php
-//get_sidebar();
 get_footer();
