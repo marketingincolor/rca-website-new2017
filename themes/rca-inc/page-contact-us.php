e942f92794b7028d05ad2e727aa10fbdf1ecb4ea
Adam Doe <adoe@marketingincolor.com>
2017-10-24 16:12:05 -0400

    Cleaned up contact page

--- themes/rca-inc/page-contact-us.php
+++ themes/rca-inc/page-contact-us.php
@@ -1,18 +1,24 @@
 <?php
 
+/**
+ * Purpose: For displaying the contact page
+ * Date: 10/24/2017
+ * Author: AD.,NB.,ET., MARKETING IN COLOR
+ */
+
 global $post;
 $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 
 get_header(); ?>
 	
-	<!-- Featured Image -->
+	<!-- FEATURED IMAGE -->
 	<div id="featured-img-wrapper" class="row expanded">
 		<div id="featured-img" style="background: linear-gradient(rgba(196,97,43, 0.7), rgba(196,97,43, 0.7)),
 	            rgba(196,97,43,0.7) url('<?php echo $backgroundImg[0]; ?>'); background-size: cover;">
 				<div class="featured-img-title"><h1><?php the_title(); ?></h1></div>
 		</div>
 	</div>
-	<!-- / Featured Image -->
+	<!-- / FEATURED IMAGE -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main contact-page">
@@ -25,7 +31,7 @@ get_header(); ?>
 			endwhile; // End of the loop.
 			?>
 
-
+			<!-- SHARE BAR -->
 			<div id="share-bar" class="row expanded show-for-medium">
 				<div class="row text-center a2a_kit">
 					<p>Share on Social Media</p>
@@ -36,18 +42,24 @@ get_header(); ?>
 					<a class="a2a_dd" title="Share"><i class="fa fa-share" aria-hidden="true"></i></a>
 				</div>
 			</div>
+			<!-- /SHARE BAR -->
 
+			<!-- SHARE BUTTON ON MOBILE -->
 			<div id="share-section" class="row hide-for-medium">
 				<div class="small-10 small-offset-1 columns">
 					<button class="orange-btn">Share <i class="fa fa-share" aria-hidden="true"></i></button>
 				</div>
 			</div>
+			<!-- /SHARE BUTTON ON MOBILE -->
 	
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
+
+	<!-- WHITE FORM -->
 	<div id="contact-learn-more-wrapper">
 		<?php get_template_part('template-parts/section', 'learn-more-form-container-white'); ?>
 	</div>
+	<!-- /WHITE FORM -->
+
 <?php
-//get_sidebar();
 get_footer();
