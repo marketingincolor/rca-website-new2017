fd3c3581f837feae9673015818d901db4547551d
Adam Doe <adoe@marketingincolor.com>
2017-10-24 17:02:38 -0400

    Unused templates

--- themes/rca-inc/template-parts/button-share.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<div class="row">
-		<div class="col-12">
-			<button class="share-btn">Share <i class="fa fa-share" aria-hidden="true"></i></button>
-		</div>
-	</div>
-</div>
