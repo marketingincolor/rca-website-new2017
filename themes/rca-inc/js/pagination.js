14c14083af1c3de0262c4d7aed573a04f0975b03
Adam Doe <adoe@marketingincolor.com>
2017-10-24 16:53:23 -0400

    Deleted unused JS file.

--- themes/rca-inc/js/pagination.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$('.pagination .current:hover, .pagination .next:hover, .pagination .prev:hover').on('hover', function() {
-		$('.navigation i').css('color', 'white');
-	});
-});
