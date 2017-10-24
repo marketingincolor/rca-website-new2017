67ee89ed1a86b709127d1c938eef9ff1f32a2184
Adam Doe <adoe@marketingincolor.com>
2017-10-24 14:43:32 -0400

    Added explanation for unused template.

--- themes/rca-inc/individual-staff-member.php
+++ themes/rca-inc/individual-staff-member.php
@@ -1,6 +1,11 @@
 <?php
-/* Template Name: Individual Staff Page */
 
+/**
+ *  STATUS: NOT IN USE
+ *  DATE: 10/24/2017
+ * 	REASON: WHEN THE THEME WAS ORIGINALLY BUILT IT USED WP USERS TO BUILD STAFF PAGES.
+ * 	THIS HAS BEEN CHANGED TO USE THE STAFF CPT. THIS HAS BEEN LEFT IN THE THEME FOR ARCHIVE PURPOSES.
+ */
 
 get_header(); ?>
 	
@@ -14,7 +19,6 @@ get_header(); ?>
 					<?php
 					while ( have_posts() ) : the_post();
 					
-
 					$staff_id = get_field('staff_id');
 					$staff_data = get_userdata($staff_id);
 					$staff_meta = get_user_meta($staff_id);
@@ -24,41 +28,30 @@ get_header(); ?>
 					$email = $staff_data->user_email;
 					$position = get_field('position', $staff_data);
 
-					//$position = $staff_meta->position;
-					// echo $full_name;
-					// echo $email;
-					// echo $position;
-
-
         			if( has_post_thumbnail()): ?>
-        					<div id="individual-staff-wrapper" class="">
-        					<img src="<?php echo the_post_thumbnail_url(); ?>" align="left"/>
-        					<h1 style="display:inline-block;"><?php echo $full_name; ?></h1><br />
-        					<h2 style="display:inline;"><?php echo $position; ?></h2><br />
-        					<div id="individual-email" class="text-left medium-text-center"><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto: <?php echo $email; ?>"> <?php echo $email; ?></a></div>
-        						
-	        				<?php the_content(); ?>
-        					</div>
+    					<div id="individual-staff-wrapper" class="">
+    					<img src="<?php echo the_post_thumbnail_url(); ?>" align="left"/>
+    					<h1 style="display:inline-block;"><?php echo $full_name; ?></h1><br />
+    					<h2 style="display:inline;"><?php echo $position; ?></h2><br />
+    					<div id="individual-email" class="text-left medium-text-center"><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto: <?php echo $email; ?>"> <?php echo $email; ?></a></div>
+    						
+        				<?php the_content(); ?>
+    					</div>
         			<?php else: ?>
-        					<div id="individual-staff-wrapper" class="">
-	        					<h1 style="display:inline-block;"><?php echo $full_name; ?></h1><br />
-	        					<h2 style="display:inline-block;"><?php echo $position; ?></h2><br />
-	        					<div id="individual-email" class="text-left medium-text-center"><i class="fa fa-envelope" aria-hidden="true"></i> <?php echo $email; ?></div>
-	        						
-		        				<?php the_content(); ?>
-        					</div>
-        			<?php endif; ?>
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
 
-					endwhile; // End of the loop.
-					?>
+    					<div id="individual-staff-wrapper" class="">
+        					<h1 style="display:inline-block;"><?php echo $full_name; ?></h1><br />
+        					<h2 style="display:inline-block;"><?php echo $position; ?></h2><br />
+        					<div id="individual-email" class="text-left medium-text-center"><i class="fa fa-envelope" aria-hidden="true"></i> <?php echo $email; ?></div>
+	        				<?php the_content(); ?>
+    					</div>
+
+        			<?php endif; ?>
+					<?php endwhile; ?>
 
 				</div>
 			</div>
-
-			
-		</main><!-- #main -->
+		</main>
 
 		<!-- LEARN MORE -->
 		<div class="row-expanded">
@@ -66,9 +59,7 @@ get_header(); ?>
 		</div>
 		<!-- END LEARN MORE -->
 		
-	</div><!-- #primary -->
-
+	</div>
 <?php
-//get_template_part('template-parts/section', 'learn-more' );
 get_template_part('template-parts/section', 'news');
 get_footer();
