edd6192628ef40cdb0379c2b137258c9c9c4c2af
Adam Doe <adoe@marketingincolor.com>
2017-10-30 11:27:13 -0400

    Fixed buttons on white papers

--- themes/rca-inc/template-parts/post-type-white-papers.php
+++ themes/rca-inc/template-parts/post-type-white-papers.php
@@ -39,6 +39,14 @@ $references_text  = get_field('references_text');
 	<?php get_template_part('template-parts/mobile', 'download-share'); ?>
 	<!-- /share/download -->
 
+	<!-- Button -->
+	<div class="row">
+		<div class="small-10 small-offset-1 columns show-for-medium">
+			<a href="<?php echo $pdf; ?>" title="Download White Paper" target="_blank"><button style="margin: 0rem 0rem 1.5rem; width: auto;" class="text-left"><i class="fa fa-download" aria-hidden="true"></i> Download</button></a>
+		</div>
+	</div>
+	<!-- /Button -->
+
 	<!-- Content -->
 	<div id="expertise-content-wp" class="row">
 		<div class="small-10 small-offset-1 columns" >
@@ -60,7 +68,7 @@ $references_text  = get_field('references_text');
 	<!-- Button -->
 	<div class="row">
 		<div class="text-center show-for-medium">
-			<a href="<?php echo $pdf; ?>" title="Download White Paper" target="_blank"><button style="margin: 0rem 0rem 2.5rem; width: auto;"><i class="fa fa-download" aria-hidden="true"></i> Download</button></a>
+			<a href="<?php echo $pdf; ?>" title="Download White Paper" target="_blank"><button style="margin: 2.5rem 0rem; width: auto;"><i class="fa fa-download" aria-hidden="true"></i> Download</button></a>
 		</div>
 	</div>
 	<!-- /Button -->
